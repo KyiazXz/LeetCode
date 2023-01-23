321bbe8ac6ead6d8e19d6ce9580a2f7f6e728188
KYIAZBEK ZHOLDOSHEV <111270349+KyiazXz@users.noreply.github.com>
2023-01-23 13:33:12 +0300

    done

---
+++ 342PowerofFour.js
@@ -0,0 +1,12 @@
+// Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+// An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+ 
+
+// Example 1:
+
+// Input: n = 16
+// Output: true
+
+// solution
